6619896621f0f56e356f643a1d31e246c8f6f100
mratin <mratin@gmail.com>
2020-11-22 20:39:27 +0100

    Calculate new cases/deaths

--- src/store/countryDays/countryDays.slice.ts
+++ src/store/countryDays/countryDays.slice.ts
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import { getCountryDays } from '../../api/covid19api';
 import { DayOneCountryDto } from '../../api/dto';
+import * as _ from 'lodash';
 
 export interface CountryDays {
     days: CountryDay[]
@@ -10,6 +11,8 @@ export interface CountryDay {
     totalCases: number;
     totalDeaths: number;
     dateTime: string;
+    newCases: number;
+    newDeaths: number;
 }
 
 interface CountryDaysState {
@@ -31,11 +34,31 @@ export const fetchCountryDays = createAsyncThunk<CountryDaysResponse, string>(
     })
 );
 
-const adapter: (dto: DayOneCountryDto) => CountryDay = (dto) => ({
+const dayOneCountryDtoAdapter: (dto: DayOneCountryDto) => CountryDay = (dto) => ({
     totalCases: dto.Confirmed,
     totalDeaths: dto.Deaths,
-    dateTime: dto.Date
+    dateTime: dto.Date,
+    newCases: 0,
+    newDeaths: 0
 });
+
+const countryDaysAdapter: (days: CountryDay[]) => CountryDay[] = (days) => {
+    if (days.length > 0) {
+        const withNews = _.zip(days, days.slice(1)).slice(0, -1).map(pair => {
+            const current: CountryDay = pair[1]!;
+            const prev: CountryDay = pair[0]!;
+            return ({
+                ...current, 
+                newCases: current.totalCases - prev.totalCases,
+                newDeaths: current.totalDeaths - prev.totalDeaths
+            });
+        });
+        return [days[0], ...withNews];
+    } else {
+        return [];
+    }
+};
+
 
 const slice = createSlice({
     name: 'countryDays',
@@ -46,11 +69,11 @@ const slice = createSlice({
             ({
                 ...state,
                 [action.payload.slug]: ({
-                    days: action.payload.dtos.map(adapter)
+                    days: countryDaysAdapter(action.payload.dtos.map(dayOneCountryDtoAdapter))
                 })
             }));
-        }
-    });
+    }
+});
 
 export const countryDaysReducer = slice.reducer;
 export const { actions } = slice;
