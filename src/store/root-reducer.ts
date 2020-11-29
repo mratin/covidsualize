f22b90cd920be21ceb96d0c42729c3bbc078cd29
mratin <mratin@gmail.com>
2020-11-29 09:17:01 +0100

    Add selection slice

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { countriesReducer } from './countries/countries.slice';
 import { countryDaysReducer } from './countryDays/countryDays.slice';
+import { selectionReducer } from './selection/selection.slice';
 
 const rootReducer = combineReducers({
   countries: countriesReducer,
-  countryDays: countryDaysReducer
+  countryDays: countryDaysReducer,
+  selection: selectionReducer
 });
 
 export default rootReducer; 
